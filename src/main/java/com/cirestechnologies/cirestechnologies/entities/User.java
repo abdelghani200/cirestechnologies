c6cff3818bc804c40c392b22982b040c6e95e502
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-03-28 23:11:27 +0000

    entity user created

--- src/main/java/com/cirestechnologies/cirestechnologies/entities/User.java
+++ src/main/java/com/cirestechnologies/cirestechnologies/entities/User.java
@@ -1,4 +1,38 @@
 package com.cirestechnologies.cirestechnologies.entities;
 
+import com.cirestechnologies.cirestechnologies.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String city;
+    private String country;
+    private String avatar;
+    private String company;
+    private String jobPosition;
+    private String mobile;
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+
+
 }
