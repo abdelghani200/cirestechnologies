8b609c06b0bb12dc08c7e6946be35dfa60bd829e
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-03-28 23:23:21 +0000

    update

--- src/main/java/com/cirestechnologies/cirestechnologies/services/Interfaces/JsonFileService.java
+++ src/main/java/com/cirestechnologies/cirestechnologies/services/Interfaces/JsonFileService.java
@@ -1,7 +1,6 @@
 package com.cirestechnologies.cirestechnologies.services.Interfaces;
 
 import com.cirestechnologies.cirestechnologies.dtos.resp.UploadRes;
-import org.apache.coyote.BadRequestException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
